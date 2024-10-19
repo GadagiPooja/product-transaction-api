1dd6f6d79d554182ee8e6ede57fd6caafc957b5f
GadagiPooja <gadagipooja16@gmail.com>
2024-10-19 11:39:53 +0530

    second

---
+++ routes/transactionRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const {
+    initializeDatabase,
+    listTransactions,
+    getStatistics,
+    getPriceRangeData,
+    getCategoryData,
+    getCombinedData
+} = require('../controllers/transactionController');
+
+router.get('/initialize', initializeDatabase);
+// router.get('/initialize', initializeDatabase);
+router.get('/transactions', listTransactions);
+router.get('/statistics', getStatistics);
+router.get('/bar-chart', getPriceRangeData);
+router.get('/pie-chart', getCategoryData);
+router.get('/combined', getCombinedData);
+
+module.exports = router;
